a500c2dd9edf3b6e82535f775906df5b87216617
Gokul <gokul.evuri@gmail.com>
2020-05-03 22:33:28 +0200

    Mouse Action

--- src/main/java/typeItAway.java
+++ src/main/java/typeItAway.java
@@ -1,5 +1,59 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+
 public class typeItAway {
 
+
+    /**
+     * Golden Rule. KISS
+     *
+     * Move mouse pointer to the right pixel
+     *      -Left Click
+     *
+     * Read the Screen
+     *      - Get chars from OCR
+     *
+     *  Use Robot to type
+     *      - Use random
+     *
+     **/
+
+    /***
+     * Global
+     ***/
+
+    int TYPE_AREA_COORDINATE_X = 200;
+    int TYPE_AREA_COORDINATE_Y = 390;
+
+    Robot robot;
+
+
+    public typeItAway()
+    {
+        try {
+            this.robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doIt()
+    {
+        mouseAction(this.robot);
+    }
+
+    public void mouseAction(Robot lRobot)
+    {
+        lRobot.mouseMove(TYPE_AREA_COORDINATE_X,TYPE_AREA_COORDINATE_Y);
+        lRobot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        lRobot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    public static void main(String args[])
+    {
+        typeItAway fakeIt = new typeItAway();
+        fakeIt.doIt();
+    }
 
 
 
